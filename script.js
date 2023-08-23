07a0735b93696b5259bd2d8ac50da057f85b4dd1
jcouper <38451393+jcouper@users.noreply.github.com>
2023-08-23 11:41:33 -0600

    refactor

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const addToCartButtons = document.querySelectorAll(".add-to-cart");
   const cartLink = document.getElementById("cart");
-  let cartItems = 0;
-
-  addToCartButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      cartItems++;
-      updateCart();
-    });
-  });
-
-  function updateCart() {
-    cartLink.textContent = `Cart (${cartItems})`;
-  }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const addToCartButtons = document.querySelectorAll(".add-to-cart");
-  const cartLink = document.getElementById("cart");
+  const cartItemsCount = document.getElementById("cart-items-count");
   let cartItems = 0;
 
   addToCartButtons.forEach(button => {
@@ -29,17 +13,23 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   function updateCart() {
-    cartLink.textContent = `Cart (${cartItems})`;
+    cartLink.textContent = `View Cart`;
+    cartItemsCount.textContent = cartItems;
+    if (cartItems === 1) {
+      cartLink.textContent = `View Cart (1 item)`;
+    } else if (cartItems > 1) {
+      cartLink.textContent = `View Cart (${cartItems} items)`;
+    }
   }
 
   function animateAddToCart() {
     const addedText = document.createElement("span");
-    addedText.textContent = "Item Added to Cart!";
+    addedText.textContent = "Added to Cart!";
     addedText.classList.add("added-to-cart");
     document.body.appendChild(addedText);
 
     setTimeout(() => {
       addedText.remove();
-    }, 2000); // Remove the added text after 2 seconds
+    }, 2000);
   }
 });
