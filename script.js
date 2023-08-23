e59cdca9952da35a6b2375eeaa605423b02d2cfa
jcouper <38451393+jcouper@users.noreply.github.com>
2023-08-23 13:59:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const addToCartButtons = document.querySelectorAll(".add-to-cart");
   const cartLink = document.getElementById("cart");
-  const cartItemsCount = document.getElementById("cart-items-count");
-  const checkoutForm = document.getElementById("checkout-form");
-  const clearCartButton = document.getElementById("clear-cart-btn");
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("email");
-
-  if (!cartLink) {
-    console.error("cartLink element not found.");
-    return;
-  }
+  let cartItemsCount = 0;
 
   updateCart();
 
-  addToCartButtons.forEach((button, index) => {
+  addToCartButtons.forEach(button => {
     button.addEventListener("click", () => {
-      addToCart(index);
+      cartItemsCount++;
+      updateCart();
       animateAddToCart();
     });
   });
 
-  checkoutForm.addEventListener("submit", event => {
-    event.preventDefault();
-    const name = nameInput.value;
-    const email = emailInput.value;
-    if (cartItemsCount > 0 && name && email) {
-      alert(`Thank you, ${name}! Your order has been placed.`);
-      clearCart();
-      updateCart();
-    } else {
-      alert("Please add items to the cart and provide your name and email.");
-    }
-  });
-
-  clearCartButton.addEventListener("click", () => {
-    clearCart();
-    updateCart();
-  });
-
-  function addToCart(itemIndex) {
-    const cart = getSessionCart();
-    cart.push(itemIndex);
-    setSessionCart(cart);
-    updateCart();
-  }
-
-  function clearCart() {
-    setSessionCart([]);
-    cartItemsCount.textContent = 0; // Reset cart items count to 0
-  }
-
-  function getSessionCart() {
-    const cartJSON = sessionStorage.getItem("cartItems");
-    return cartJSON ? JSON.parse(cartJSON) : [];
-  }
-
-  function setSessionCart(cart) {
-    const cartJSON = JSON.stringify(cart);
-    sessionStorage.setItem("cartItems", cartJSON);
-  }
-
   function updateCart() {
-    const cart = getSessionCart();
-    const cartItemCount = cart.length;
-    cartItemsCount.textContent = cartItemCount;
-    updateCartLinkText(cartItemCount);
-  }
-
-  function updateCartLinkText(cartItemCount) {
-    if (cartLink) {
-      if (cartItemCount === 1) {
-        cartLink.textContent = `View Cart (1 item)`;
-      } else {
-        cartLink.textContent = `View Cart (${cartItemCount} items)`;
-      }
+    if (cartItemsCount === 1) {
+      cartLink.textContent = `View Cart (1 item)`;
+    } else {
+      cartLink.textContent = `View Cart (${cartItemsCount} items)`;
     }
   }
 
