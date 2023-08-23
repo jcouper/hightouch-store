929aed6cc099275ac4a7b5152f623191e5d6bc0b
jcouper <38451393+jcouper@users.noreply.github.com>
2023-08-23 11:36:08 -0600

    add to cart animation

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ document.addEventListener("DOMContentLoaded", () => {
     cartLink.textContent = `Cart (${cartItems})`;
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addToCartButtons = document.querySelectorAll(".add-to-cart");
+  const cartLink = document.getElementById("cart");
+  let cartItems = 0;
+
+  addToCartButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      cartItems++;
+      updateCart();
+      animateAddToCart();
+    });
+  });
+
+  function updateCart() {
+    cartLink.textContent = `Cart (${cartItems})`;
+  }
+
+  function animateAddToCart() {
+    const addedText = document.createElement("span");
+    addedText.textContent = "Item Added to Cart!";
+    addedText.classList.add("added-to-cart");
+    document.body.appendChild(addedText);
+
+    setTimeout(() => {
+      addedText.remove();
+    }, 2000); // Remove the added text after 2 seconds
+  }
+});
